2c1204fe304b3253bb177d2c7e7a9f1d8a5ae499
Kenzo ARAI <blue_tabby@mac.com>
2023-04-07 04:27:55 +0900

    Added ISO-2022-JP Test

---
+++ lib/iso_2022_jp.dart
@@ -0,0 +1,10 @@
+/// Provides missing ISO-2022-JP character encoder / decoder / codec /
+/// encoding / charset converters for Dart.
+///
+/// Use the following statement to access only the big5 codec:
+/// ```dart
+/// import 'package:enough_convert/iso_2022_jp.dart';
+/// ```
+library enough_convert_iso_2022_jp;
+
+export 'src/iso_2022_jp/iso_2022_jp.dart';
